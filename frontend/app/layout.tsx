7f848057f6573bf2c06f843c68bc777a3a613ea0
MariaMonti <mar.u1998@hotmail.com>
2026-07-29 11:51:18 -0300

    WIP: Maquetado de vista Benchmark y tokens

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
+
+const redHatDisplay = Red_Hat_Display({ 
+  subsets: ["latin"],
+  variable: "--font-redhat" // Opcional, para usarla en Tailwind
+});
 
 export const metadata: Metadata = {
   title: "[Nombre del proyecto]",
@@ -10,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className={redHatDisplay.className}>{children}</body>
     </html>
   );
 }
