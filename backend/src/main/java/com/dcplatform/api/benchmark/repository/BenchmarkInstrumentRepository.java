04db58af1af9980a367e92f51b11fed0ac714efa
Héctor Armando Cortez <updown.input@gmail.com>
2026-08-29 16:26:27 -0300

    feat (backend): repositorio perteneciente al instrumento (Benchmark)

---
+++ backend/src/main/java/com/dcplatform/api/benchmark/repository/BenchmarkInstrumentRepository.java
@@ -0,0 +1,14 @@
+package com.dcplatform.api.benchmark.repository;
+
+import com.dcplatform.api.benchmark.model.BenchmarkInstrument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BenchmarkInstrumentRepository extends JpaRepository<BenchmarkInstrument, UUID> {
+
+	Optional<BenchmarkInstrument> findByIsActiveTrue();
+}
