60e60192900e77df86b104aba2e927f2cc4b5a50
Héctor Armando Cortez <updown.input@gmail.com>
2026-08-29 16:37:48 -0300

    feat (backend): getActiveInstrument implementado

--- backend/src/main/java/com/dcplatform/api/benchmark/service/BenchmarkServiceImpl.java
+++ backend/src/main/java/com/dcplatform/api/benchmark/service/BenchmarkServiceImpl.java
@@ -1,14 +1,28 @@
 package com.dcplatform.api.benchmark.service;
 
 import com.dcplatform.api.benchmark.model.dto.*;
+import com.dcplatform.api.benchmark.repository.BenchmarkInstrumentRepository;
+import com.dcplatform.api.benchmark.service.mapper.BenchmarkMapper;
+import com.dcplatform.api.shared.ApiException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BenchmarkServiceImpl implements BenchmarkService {
 
+	private final BenchmarkMapper mapper;
+	private final BenchmarkInstrumentRepository instrumentRepository;
+
+	public BenchmarkServiceImpl(BenchmarkMapper mapper,
+	                            BenchmarkInstrumentRepository instrumentRepository) {
+		this.mapper = mapper;
+		this.instrumentRepository = instrumentRepository;
+	}
+
 	@Override
 	public ActiveBenchmarkResponse getActiveInstrument() {
-		return null;
+		return instrumentRepository.findByIsActiveTrue()
+				.map(mapper::toActiveBenchmarkResponse)
+				.orElseThrow(() -> ApiException.notFound("No se encontró un instrumento de benchmark activo."));
 	}
 
 	@Override
