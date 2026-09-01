a4f8949a2b0e9cc76841ea00c9729ae93e925d27
Héctor Armando Cortez <updown.input@gmail.com>
2026-08-31 22:03:05 -0300

    fix (backend): buscar por lead, response y status para asegurar una respuesta única

--- backend/src/main/java/com/dcplatform/api/benchmark/repository/BenchmarkResponseRepository.java
+++ backend/src/main/java/com/dcplatform/api/benchmark/repository/BenchmarkResponseRepository.java
@@ -2,6 +2,7 @@ package com.dcplatform.api.benchmark.repository;
 
 import com.dcplatform.api.benchmark.model.BenchmarkInstrument;
 import com.dcplatform.api.benchmark.model.BenchmarkResponse;
+import com.dcplatform.api.benchmark.model.BenchmarkResponse.BenchmarkStatus;
 import com.dcplatform.api.leads.model.LeadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,9 @@ import java.util.UUID;
 @Repository
 public interface BenchmarkResponseRepository extends JpaRepository<BenchmarkResponse, UUID> {
 
-	Optional<BenchmarkResponse> findByLeadAndInstrument(LeadEntity lead, BenchmarkInstrument instrument);
+	Optional<BenchmarkResponse> findByLeadAndInstrumentAndStatusWithinIgnoreCase(LeadEntity lead,
+	                                                                             BenchmarkInstrument instrument,
+	                                                                             BenchmarkStatus status);
 
 	Optional<BenchmarkResponse> findByIdAndLead(UUID responseId, LeadEntity lead);
 }
